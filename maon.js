7c18cdda0bbbe92011da846875c2bb28846a473d
Yosef mare <yosefmare108@gmail.com>
2023-08-04 15:34:14 +0300

    updating user

--- maon.js
+++ maon.js
@@ -1,10 +1,10 @@
 const url = "https://jsonplaceholder.typicode.com/"
 
-async function getUsers(){
+async function getUsers() {
     const response = await fetch(`${url}users`);
     const data = await response.json()
     data.forEach(user => {
-        document.getElementById('users').innerHTML +=`
+        document.getElementById('users').innerHTML += `
         <!-- user info card -->
         <div class="user-detels">
             <div class="info">
@@ -16,7 +16,7 @@ async function getUsers(){
             <div class="user-btn">
                 <button class="proses-btn more-info" onmouseover="showMoreData(this)" onclick = "collapseMoreData(this)">Outer Data</button>
                 <div>
-                    <button class="proses-btn update">update</button>
+                    <button class="proses-btn update" onclick = "updateUser(this)">update</button>
                     <button class="proses-btn delete" onclick= "deleteUser(this)">delete</button>
                 </div>
             </div>
@@ -32,22 +32,49 @@ async function getUsers(){
     });
 }
 
-function showMoreData(e){
+function showMoreData(e) {
     e.parentElement.parentElement.childNodes[5].style.display = ""
 }
 
-function collapseMoreData(e){
+function collapseMoreData(e) {
     e.parentElement.parentElement.childNodes[5].style.display = "none"
 }
 
-async function deleteUser(e){
+async function deleteUser(e) {
     const userId = e.parentElement.parentElement.parentElement.childNodes[1].childNodes[1].childNodes[2].value;
-    const response = await fetch(`${url}users/${userId}`,{
-    method: "DELETE"
-})
-const data = await response.json();
-console.log(data);
+    const response = await fetch(`${url}users/${userId}`, {
+        method: "DELETE"
+    })
+    const data = await response.json();
+    console.log(`user: ${userId} Deleted`, data);
 
-e.parentElement.parentElement.parentElement.style.display = "none"
+    e.parentElement.parentElement.parentElement.style.display = "none"
+
+}
+
+async function updateUser(e) {
+    //  the user Enter the name and email values
+    const userId = e.parentElement.parentElement.parentElement.childNodes[1].childNodes[1].childNodes[2].value;
+    let newUserName = prompt("type new user name ")
+    let newUserEmail = prompt("type new user email ")
+
+    //  the default user and email values
+    e.parentElement.parentElement.parentElement.childNodes[1].childNodes[3].childNodes[2].value = newUserName;
+    e.parentElement.parentElement.parentElement.childNodes[1].childNodes[5].childNodes[2].value = newUserEmail;
+    // call API url to update
+    const response = await fetch(`${url}users/${userId}`, {
+        method: 'PATCH',
+        body: JSON.stringify({
+            name: newUserName,
+            email: newUserEmail
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+
+    const data = await response.json();
+    console.log(data); 
+
 
 }
